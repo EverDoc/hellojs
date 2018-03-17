b2543e70850e819b12103048e21565268264406e
baikangwang <baikangwang@hotmail.com>
2018-03-17 20:48:59 +0800

    complete switch

--- story.js
+++ story.js
@@ -13,11 +13,16 @@ console.log("control-flow: if")
 for (let index = 0; index < arrStory.length; index++) {
     const strStory = arrStory[index];
     const key=strStory.substr(0,strStory.indexOf(":"));
-    if(key ==="Given"){
-        console.log(`It's a given: ${strStory.substr(strStory.indexOf(":")+1)}`);
-    }else if(key === "When"){
-        console.log(`It's a when: ${strStory.substr(strStory.indexOf(":")+1)}`);
-    }else{
-        console.log(`It's a ${key}: ${strStory.substr(strStory.indexOf(":")+1)}`);
+    switch (key) {
+        case "Given":
+            console.log(`It's a given: ${strStory.substr(strStory.indexOf(":")+1)}`);
+            break;
+        case "When":
+            console.log(`It's a when: ${strStory.substr(strStory.indexOf(":")+1)}`);
+            break;
+        case "Then":
+        default:
+            console.log(`It's a ${key}: ${strStory.substr(strStory.indexOf(":")+1)}`);
+            break;
     }
 }
