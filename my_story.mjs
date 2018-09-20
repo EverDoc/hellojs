2949d24c6fe32edc78efc004998ca477d49a3fca
baikangwang <baikangwang@hotmail.com>
2018-09-20 12:38:54 +0800

    capture approve proof demo

--- my_story.mjs
+++ my_story.mjs
@@ -16,19 +16,32 @@ export default class MyStory extends Story{
     Then(context){
         super.Then(context);
         let driver= new webdriver.Builder().forBrowser("chrome").build();
-        const msg_url='https://raw.githubusercontent.com/EverDoc/hellojs/webdriver/mom.txt';
+        const msg_url='https://navink.navitor.com/home/index/?BrokerConfigID=eafac950-eca4-4eae-8f18-ca49227c0339';
         driver.get(msg_url);
-        driver.wait(webdriver.until.urlIs(msg_url), 1000*10)
+        driver.wait(webdriver.until.elementLocated(webdriver.By.css('iframe[id="productConfigFrame"]')) , 1000*30)
         .then((success)=>{
-            driver.findElement(webdriver.By.tagName('pre')).getText().then((message)=>{
-                    driver.quit();
-                    console.log(message);       
+            console.log('++found iframe',success);
+            driver.switchTo().frame(0).then(success=>{
+                driver.wait(webdriver.until.elementLocated(webdriver.By.css('input[id="addToCartBtnTop"]')) , 1000*30)
+                .then(success=>{
+                    console.log('++found button',success);
+                    driver.findElement(webdriver.By.css('input[id="addToCartBtnTop"]')).getAttribute("value")
+                    .then((message)=>{
+                        driver.quit();
+                        console.log(message);       
+                    });
                 }
-            );
-            },(reason)=>{
-                // do nothing
-                console.log(reason);
-                driver.quit();
+                ,(reason)=>{
+                    // do nothing
+                    console.log(reason);
+                    driver.quit();
+                });
+            });
+        }
+        ,(reason)=>{
+            // do nothing
+            console.log(reason);
+            driver.quit();
         });
     }
 }
